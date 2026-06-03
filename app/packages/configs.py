8250bccd6341ba3531d4bdd5b7b2ef8842784f26
Damir Islyamov <islmvdmr@gmail.com>
2026-06-03 23:46:15 +0400

    fix configs

--- app/packages/configs.py
+++ app/packages/configs.py
@@ -4,11 +4,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
-    APP_NAME: str = "async-tasks-service"
-    APP_HOST: str = "127.0.0.1"
+    APP_NAME: str = "task-service"
+    APP_HOST: str = "0.0.0.0"
     APP_PORT: int = 8000
     DEBUG: bool = True
 
+    DATABASE_URL: str
+    AMQP_URL: str
+
+    TASK_QUEUE_NAME: str = "tasks"
+    TASK_QUEUE_MAX_PRIORITY: int = 10
+
+    MAX_CONCURRENT_PROCESSES: int = 1
+
+    TIME_TO_WAIT_TASK: int = 10
 
 
-settings = Settings()
+settings = Settings()  # type: ignore
